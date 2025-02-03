2df678329bd98cb01c3dd078b663dcd1da5493c7
Zachran Razendra <zachranraze@recodex.id>
2025-02-03 17:55:25 +0700

    Refactor profile edit layout for improved structure and readability

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,19 +1,22 @@
 <x-dashboard-layout>
-    <div class="mx-auto sm:px-6 lg:px-8 space-y-6">
-        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-            <div class="max-w-xl">
+    <div class="space-y-6">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <div>
+                <h1 class="text-2xl font-semibold text-shark-950">Profile</h1>
+                <p class="mt-1 text-sm text-shark-500">Manage your profile information</p>
+            </div>
+        </div>
+
+        <div class="grid lg:grid-cols-2 gap-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @include('profile.partials.update-profile-information-form')
             </div>
-        </div>
 
-        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-            <div class="max-w-xl">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @include('profile.partials.update-password-form')
             </div>
-        </div>
 
-        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-            <div class="max-w-xl">
+            <div class="col-span-2 p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @include('profile.partials.delete-user-form')
             </div>
         </div>
